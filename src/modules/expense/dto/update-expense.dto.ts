6899fc555ee3c032f68db63a6201217c36f1de69
ivan <ivantorres22_8@hotmail.com>
2025-06-03 13:36:00 -0500

    feat(expense): actualizar el DTO de actualización de gastos para incluir nuevos campos opcionales. Se añadieron 'proyectId', 'categoryId', 'data', 'total', y 'status', mejorando la flexibilidad y la capacidad de gestión de gastos.

--- src/modules/expense/dto/update-expense.dto.ts
+++ src/modules/expense/dto/update-expense.dto.ts
@@ -1,9 +1,32 @@
-import { PartialType } from '@nestjs/swagger'
-import { CreateExpenseDto } from './create-expense.dto'
-import { IsOptional, IsString } from 'class-validator'
+import { IsString, IsOptional, IsEnum, IsNumber } from 'class-validator'
+import { ExpenseStatus } from '../entities/expense.entity'
 
-export class UpdateExpenseDto extends PartialType(CreateExpenseDto) {
-  @IsOptional()
+export class UpdateExpenseDto {
   @IsString()
+  @IsOptional()
+  proyectId?: string
+
+  @IsString()
+  @IsOptional()
+  categoryId?: string
+
+  @IsString()
+  @IsOptional()
+  data?: string
+
+  @IsNumber()
+  @IsOptional()
+  total?: number
+
+  @IsString()
+  @IsOptional()
   fechaEmision?: string
+
+  @IsEnum(['pending', 'approved', 'rejected'])
+  @IsOptional()
+  status?: ExpenseStatus
+
+  @IsString()
+  @IsOptional()
+  companyId?: string
 }
